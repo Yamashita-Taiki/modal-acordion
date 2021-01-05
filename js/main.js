31901144e09d09e474369c0cee6607b6a87b586a
Yamashita-Taiki <0809taikiyamashita@gmail.com>
2021-01-05 23:11:04 +0900

    htmlに追加

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
-// const menu = document.querySelectorAll('js-menu');
-// function toggle(){
-//   const content = this.nextElementSibling;
-//   this.classList.toggle('is-active');
-//   content.classList.toggle("is-open");
-// };
-// for (let i = 0; i < menu.length; i++) {
-//   menu[i].addEventListener("click", toggle);
-// }
-// const btn = document.getElementById('btn');
-// const modal = document.getElementById('modal');
+const menu = document.querySelectorAll('.js-menu');
+function toggle(){
+  const content = this.nextElementSibling;
+  this.classList.toggle('is-active');
+  content.classList.toggle("is-open");
+};
+for (let i = 0; i < menu.length; i++) {
+  menu[i].addEventListener("click", toggle);
+}
+const btn = document.getElementById('btn');
+const modal = document.getElementById('modal');
 
-// btn.addEventListener('click', function() {
-//   modal.style.display = 'block';
-// }
+btn.addEventListener('click', function() {
+  modal.style.display = 'block';
+})
 
-// const closeBtn = document.getElementById('closeBtn');
+const closeBtn = document.getElementById('closeBtn');
 
-// closeBtn.addEventListener('click', function() {
-//   modal.style.display = 'none';
-// })
+closeBtn.addEventListener('click', function() {
+  modal.style.display = 'none';
+})
